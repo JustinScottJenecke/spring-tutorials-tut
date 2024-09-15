ae1e000a1bdddbb0b367f7617470cbd524a2d36e
justinscottjenecke <justinjenecke@gmail.com>
2024-09-15 19:57:27 +0200

    init-commit: creates spring security project boilerplate + creates config for connecting to data source

---
+++ amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/SpringSecurityJpaTutorialApplication.java
@@ -0,0 +1,13 @@
+package io.github.justinscottjenecke.spring_security_jpa_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaTutorialApplication.class, args);
+	}
+
+}
---
+++ amigoscode/spring-security-jpa-tutorial/src/test/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/SpringSecurityJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.justinscottjenecke.spring_security_jpa_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
