40ab8dcc159fdb2c55864d843940de6fa72390c2
justinscottjenecke <justinjenecke@gmail.com>
2024-09-16 09:59:37 +0200

    feat: modifies return values of overridden implemented methods - User.java

--- amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/user/User.java
+++ amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/user/User.java
@@ -41,31 +41,35 @@ public class User implements UserDetails {
 
     @Override
     public String getPassword() {
-        return "";
+        return this.password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return this.email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        // return UserDetails.super.isAccountNonExpired();
+        return  true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        // return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        // return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        // return UserDetails.super.isEnabled();
+        return true;
     }
 }
