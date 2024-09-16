55ea804cb6264def785f06d775b33054719693d4
justinscottjenecke <justinjenecke@gmail.com>
2024-09-16 10:05:26 +0200

    feat: creates findbyemail method + creates user repository layer

---
+++ amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package io.github.justinscottjenecke.spring_security_jpa_tutorial.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface IUserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
