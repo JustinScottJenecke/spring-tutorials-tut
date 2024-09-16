78bd8bb5c5d1c98b3c859df7a451e47580cdfe56
justinscottjenecke <justinjenecke@gmail.com>
2024-09-16 13:15:11 +0200

    feat: writes function for extracting and parsing all claims from jwt payload

--- amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/config/JwtService.java
+++ amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/config/JwtService.java
@@ -1,11 +1,26 @@
 package io.github.justinscottjenecke.spring_security_jpa_tutorial.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
-    public String extractUsername(String token) {
+    public String extractUsername(String jwtToken) {
         return "";
+    }
+
+    public String getSignInKey() {
+        return "";
+    }
+
+    public Claims extractAllClaims(String jwtToken) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSignInKey()) // sets signing key from settings
+                .build()
+                .parseClaimsJws(jwtToken)  // decodes hash value reverse algorithm and parse into usable data
+                .getBody(); // return body of jwt
     }
 }
