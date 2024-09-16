7d7b1b18953e5ad9e1b1f720d5385060b773c2cd
justinscottjenecke <justinjenecke@gmail.com>
2024-09-16 09:35:41 +0200

    feat: update db logins in application.yml and adds properties and lombok to User model class

---
+++ amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/user/User.java
@@ -0,0 +1,26 @@
+package io.github.justinscottjenecke.spring_security_jpa_tutorial.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
