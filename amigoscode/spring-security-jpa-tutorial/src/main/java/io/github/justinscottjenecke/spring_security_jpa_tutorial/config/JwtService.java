8513054c14a98be3d204f57638d77a4f9330af95
justinscottjenecke <justinjenecke@gmail.com>
2024-09-16 11:15:02 +0200

    feat: creates jwt service, implments filter checking auth header in request, adds placeholder functionality for extracting username from jwt

---
+++ amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/config/JwtService.java
@@ -0,0 +1,11 @@
+package io.github.justinscottjenecke.spring_security_jpa_tutorial.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String token) {
+        return "";
+    }
+}
