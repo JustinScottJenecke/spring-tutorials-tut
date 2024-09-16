8dca3c4b04517a3109757dbb316d112ed8e3353d
justinscottjenecke <justinjenecke@gmail.com>
2024-09-16 13:30:48 +0200

    feat: sets secret key for singing jwts and adds implmentation for getSignInKey to decode secret

--- amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/config/JwtService.java
+++ amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/config/JwtService.java
@@ -2,17 +2,25 @@ package io.github.justinscottjenecke.spring_security_jpa_tutorial.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+
+    // NOTE: move to app properties
+    private static final String SECRET_KEY = "446b7e462436572867695f666a337a7031377b3e7d733929212d7e663b";
 
     public String extractUsername(String jwtToken) {
         return "";
     }
 
-    public String getSignInKey() {
-        return "";
+    public Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 
     public Claims extractAllClaims(String jwtToken) {
