eadb880eb2f6ddaca9d84372e7fa0a3d462d6eba
justinscottjenecke <justinjenecke@gmail.com>
2024-09-17 11:58:41 +0200

    feat: completes implementation for userdetails serivce, authentication filter, and security context holder

--- amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/config/JwtAuthenticationFilter.java
+++ amigoscode/spring-security-jpa-tutorial/src/main/java/io/github/justinscottjenecke/spring_security_jpa_tutorial/config/JwtAuthenticationFilter.java
@@ -1,15 +1,18 @@
 package io.github.justinscottjenecke.spring_security_jpa_tutorial.config;
 
 import io.github.justinscottjenecke.spring_security_jpa_tutorial.user.User;
+import io.jsonwebtoken.security.SecurityBuilder;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -45,8 +48,28 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractUsername(jwtToken);
 
         // if userEmail exists so incoming request has valid jwt but user has not been loaded from database
+        // or user is not yet authenticated.
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            // retrieve user from db user using email in token
             UserDetails user = this.userDetailsService.loadUserByUsername(userEmail);
+
+            if(jwtService.isTokenValid(jwtToken, user)) {
+
+                // if token is valid, generate new authentication token
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        user,
+                        null,
+                        user.getAuthorities()
+                );
+
+                // set details of auth token
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                // persist auth token inside security context
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            filterChain.doFilter(request, response);
         }
 
     }
